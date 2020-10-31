eb414a14b96800da3d9b968720fcf24fdaf47e6f
kinyo <kinyodanie@gmail.com>
2020-11-01 02:41:11 +0300

    Delete smtp.rb

--- config/smtp.rb
+++
@@ -1,9 +0,0 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch("smtp.sendgrid.net"), # example: "smtp.sendgrid.net"
-  authentication: :plain,
-  domain: ENV.fetch("heroku.com"), # example: "heroku.com"
-  enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
-  port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
-}
